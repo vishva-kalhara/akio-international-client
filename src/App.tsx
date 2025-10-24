a90d9dbd34dba11745174d661a1cfe282123ecbb
vishva-kalhara <vishvakalhara@gmail.com>
2025-10-24 20:55:10 +0530

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Nav from "./components/nav";
+import Nav from "./components/shared/nav";
 import { LanguageProvider } from "./contexts/language-ctx";
+import HeroSection from "./sections/hero";
 
 function App() {
     return (
         <LanguageProvider>
             <Nav />
+            <HeroSection />
         </LanguageProvider>
     );
 }
--- src/components/shared/nav.tsx
+++ src/components/shared/nav.tsx
@@ -1,13 +1,13 @@
 import { Phone } from "lucide-react";
-import { useLanguage } from "../hooks/use-language";
-import Button from "./units/button";
-import Link from "./units/link";
+import { useLanguage } from "../../hooks/use-language";
+import Button from "../units/button";
+import Link from "../units/link";
 
 const Nav = () => {
     const { t } = useLanguage();
 
     return (
-        <nav className="bg-beige w-full flex px-8 py-6 justify-between items-center">
+        <nav className="bg-beige w-full flex px-8 py-6 justify-between items-center border-b-2 border-b-black/5">
             <div>
                 <Link href="/">{t.shared.name}</Link>
             </div>
