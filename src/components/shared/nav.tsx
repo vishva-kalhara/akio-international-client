93c7cd66e4cb4f27b1f6d24f8de022984c853df0
vishva-kalhara <vishvakalhara@gmail.com>
2025-10-24 21:03:04 +0530

    sitemap

--- src/components/shared/nav.tsx
+++ src/components/shared/nav.tsx
@@ -2,6 +2,7 @@ import { Phone } from "lucide-react";
 import { useLanguage } from "../../hooks/use-language";
 import Button from "../units/button";
 import Link from "../units/link";
+import { siteMap } from "../../site-map";
 
 const Nav = () => {
     const { t } = useLanguage();
@@ -9,14 +10,16 @@ const Nav = () => {
     return (
         <nav className="bg-beige w-full flex px-8 py-6 justify-between items-center border-b-2 border-b-black/5">
             <div>
-                <Link href="/">{t.shared.name}</Link>
+                <Link href={siteMap.landingPage}>{t.shared.name}</Link>
             </div>
             <div className="flex gap-6 items-center">
-                <Link href="/vehicles">{t.shared.nav.linkVehicles}</Link>
-                <Link href="/home-services">
+                <Link href={siteMap.vehiclesPage}>
+                    {t.shared.nav.linkVehicles}
+                </Link>
+                <Link href={siteMap.homeServicesPage}>
                     {t.shared.nav.linkHomeServices}
                 </Link>
-                <a href="/contact-us">
+                <a href={siteMap.contactUsPage}>
                     <Button variant="Primary">
                         <Phone fill="#fff" size={18} className="mt-1" />{" "}
                         {t.shared.nav.cta}
---
+++ src/site-map.ts
@@ -0,0 +1,7 @@
+export const siteMap = {
+    landingPage: "/",
+    vehiclesPage: "/vehicles",
+    homeServicesPage: "/home-services",
+    contactUsPage: "/contact-us",
+    aboutUsPage: "/about-us",
+};
