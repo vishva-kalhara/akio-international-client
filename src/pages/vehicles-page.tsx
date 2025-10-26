31c4873f3764ba983bf06474025babafa788926d
vishva-kalhara <vishvakalhara@gmail.com>
2025-10-26 18:41:19 +0530

    refactored vehicle services

--- src/pages/vehicles-page.tsx
+++ src/pages/vehicles-page.tsx
@@ -1,10 +1,12 @@
-import VehicleServicesSection from "../sections/vehicles-services/vehicle-services.section";
+import { useLanguage } from "../hooks/use-language";
 
 const VehiclesPage = () => {
+    const { lang } = useLanguage();
+
     return (
-        <>
-            <VehicleServicesSection />
-        </>
+        <section className="bg-beige p-24 flex flex-col items-center">
+            <img src={`./vehicle-services-${lang}.svg`} className="w-full" />
+        </section>
     );
 };
 
