3b4c7e7f6babcedcc426d1bb50a6f4bcf210fcfb
vishva-kalhara <vishvakalhara@gmail.com>
2025-10-26 18:49:01 +0530

    lang translation

--- src/components/shared/nav.tsx
+++ src/components/shared/nav.tsx
@@ -1,11 +1,11 @@
-import { Phone } from "lucide-react";
+import { Languages, Phone } from "lucide-react";
 import { useLanguage } from "../../hooks/use-language";
 import Button from "../units/button";
 import Link from "../units/link";
 import { siteMap } from "../../site-map";
 
 const Nav = () => {
-    const { t } = useLanguage();
+    const { t, switchLanguage, lang } = useLanguage();
 
     return (
         <nav className="bg-beige w-full flex px-8 py-6 justify-between items-center border-b-2 border-b-black/5">
@@ -22,12 +22,24 @@ const Nav = () => {
                 >
                     {t.shared.nav.linkHomeServices}
                 </Link>
-                <a href={siteMap.contactUsPage}>
-                    <Button variant="Primary">
-                        <Phone fill="#fff" size={18} className="mt-1" />{" "}
-                        {t.shared.nav.cta}
-                    </Button>
-                </a>
+                <div className="flex gap-2 items-center ">
+                    <button
+                        className="hover:cursor-pointer p-3 rounded-full border-2 border-black/10"
+                        onClick={() =>
+                            switchLanguage(lang == "en" ? "jp" : "en")
+                        }
+                    >
+                        <Languages size={18} />
+                    </button>
+                    <a href={siteMap.contactUsPage}>
+                        <Button variant="Primary">
+                            <Phone fill="#fff" size={18} className="mt-1" />{" "}
+                            <span className="hidden sm:block">
+                                {t.shared.nav.cta}
+                            </span>
+                        </Button>
+                    </a>
+                </div>
             </div>
         </nav>
     );
